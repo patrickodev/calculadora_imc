65f0c8d286e2dd998b57fca8ead735f030cc43ca
Patrick Oliveira <patrickoliveirabrto@gmail.com>
2021-02-08 18:36:48 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Calculadora IMC',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Calculadora IMC'),
@@ -33,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,65 +34,58 @@ class _MyHomePageState extends State<MyHomePage> {
   double _IMC = 0.0;
   double altura = 0.0;
   double peso = 0.0;
-/*
-  get alturaController => null;
-  get pesoController => null;
-*/
+
+
   void _calcIMC(double p, double a) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _IMC = p / (a*a);
+      _IMC = p / (a*a/1000);
     });
+  }
+  String _text(double i) {
+
+      if(i < 18.5){
+        return 'Você está abaixo do peso.';
+      }
+      else if(i >= 18.5 && i < 25.0){
+        return 'Seu peso está normal.';
+      }
+      else if(i >= 25.0 && i < 30.0){
+        return 'Você está com pré-obesidade.';
+      }
+      else if(i >= 30.0 && i < 35.0){
+        return 'Você está com obesidade grau I.';
+      }
+      else if(i >= 35.0 && i < 40.0){
+        return 'Você está com obesidade grau II.';
+      }
+      else if(i >= 40.0) {
+        return 'Você está com obesidade grau III.';
+      }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-          TextField(
+          Padding(padding: EdgeInsets.symmetric(horizontal: 50), child: TextField(keyboardType: TextInputType.number,
             onChanged: (text){
               altura = double.parse(text);
             },
-              decoration: InputDecoration(
+            decoration: InputDecoration(
                 border: InputBorder.none,
-                hintText: 'Digite sua altura em metros'
-              ),
-          ),
-          TextField(
+                hintText: 'Digite sua altura em centimetros'
+            ),
+          ),),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 50), child:TextField(keyboardType: TextInputType.number,
               onChanged: (text){
                 peso = double.parse(text);
               },
@@ -119,7 +93,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 border: InputBorder.none,
                 hintText: 'Digite seu peso em quilos'
               ),
-          ),
+          ),),
+          Padding(padding: EdgeInsets.symmetric(vertical: 50), child:RaisedButton(
+                onPressed: ()=> _calcIMC(peso, altura),
+                child: Text(
+                    'Calcule',
+                    style: TextStyle(fontSize: 20)
+                )
+            ),),
             Text(
               'Seu IMC é:',
             ),
@@ -127,13 +108,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_IMC',
               style: Theme.of(context).textTheme.headline4,
             ),
-            RaisedButton(
-              onPressed: ()=> _calcIMC(peso, altura),
-              child: Text(
-              'Calcule',
-              style: TextStyle(fontSize: 20)
-              )
-          ),
+            Text(
+                _text(_IMC),
+            ),
           ],
         ),
       ),
